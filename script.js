c1857a44a83ccdf8ed48befe1515113585086cd9
xtrhighlights-commits <xtr.highlights@gmail.com>
2026-01-28 19:41:58 +0530

    script.js

--- script.js
+++ script.js
@@ -2,34 +2,32 @@ gsap.from(".logo",{
   y:-100,
   opacity:0,
   duration:1
-})
+});
 
 gsap.from(".title",{
   y:50,
   opacity:0,
   delay:0.5,
   duration:1
-})
+});
 
 gsap.from(".tagline",{
   y:50,
   opacity:0,
   delay:1,
   duration:1
-})
+});
 
 gsap.from(".cta",{
   scale:0,
   delay:1.5,
   duration:0.6
-})
+});
 
 gsap.from(".card",{
-  scrollTrigger:{
-    trigger:".services",
-    start:"top 80%"
-  },
   y:100,
   opacity:0,
-  stagger:0.2
-})
+  stagger:0.2,
+  duration:1,
+  delay:2
+});
