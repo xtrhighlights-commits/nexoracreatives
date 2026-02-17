0752ec8c642f4353c5441596135d34e359c78ee2
xtrhighlights-commits <xtr.highlights@gmail.com>
2026-02-17 16:08:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 const sections = document.querySelectorAll("section");
 
 window.addEventListener("scroll",()=>{
-
 sections.forEach(sec=>{
 const top = sec.getBoundingClientRect().top;
-
-if(top < window.innerHeight - 100){
-sec.style.opacity = 1;
-sec.style.transform = "translateY(0)";
+if(top < window.innerHeight-100){
+sec.style.opacity=1;
+sec.style.transform="translateY(0)";
 }
 });
-
 });
