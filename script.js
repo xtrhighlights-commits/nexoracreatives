7c305d15be74bf4ca9cfbcbe45772bb4aeee25d7
xtrhighlights-commits <xtr.highlights@gmail.com>
2026-02-17 19:20:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const currentPage = location.pathname.split("/").pop();
+const current = location.pathname.split("/").pop();
 
 document.querySelectorAll(".nav-links a").forEach(link=>{
-if(link.getAttribute("href")===currentPage){
+if(link.getAttribute("href")===current){
 link.classList.add("active");
 }
 });
